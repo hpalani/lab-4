34f45de3e8a2225d4a921007532a3b8bfcffdf67
Harish Palani <harishpalani3@gmail.com>
2017-11-09 00:37:19 +0000

    Done!

---
+++ main.cpp
@@ -0,0 +1,82 @@
+ /*
+ NAME: Harish Palani
+ CRN: 41600
+ ASSIGNMENT: Lab 4
+ SOURCES: stackoverflow.com
+ */
+
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+long long findPrimeFactorization(long long n);
+bool isPrime(long long n);
+
+int main() {
+    long long input;
+    
+    for (long long i = 0; i < 4; i++) {
+        // Accept input integer.
+        cout << "Enter input integer below." 
+             << endl;
+        cin >> input;
+        
+        if (cin.fail() || input < 1) {
+            cout << "\n"
+                 << "Make sure you're entering a positive integer!" 
+                 << endl;
+            cin >> input;
+        }
+        
+        if (isPrime(input)) {
+            cout << "\n" 
+                 << input
+                 << " is prime. "
+                 << "\n"
+                 << endl;
+        } else {
+            cout << "\n" << "Prime factorization: ";
+            findPrimeFactorization(input);
+        }
+    }
+}
+
+long long findPrimeFactorization(long long n) {
+    // First, check divisibility by 2 (only even prime)
+    while (n % 2 == 0) {
+        cout << 2
+             << " ";
+        n = n / 2;
+    }
+    
+    // Now, loop through odds until sqrt(n)
+    for (long long i = 3; i <= sqrt(n); i = i + 2) {
+        // Check divisibility by i
+        while (n % i == 0) {
+            cout << i
+                 << " ";
+            n = n / i;
+        }
+    }
+ 
+    // Finally, deal with prime numbers greater than 2
+    if (n > 2) {
+        cout << n
+             << " ";
+    }
+    
+    cout << "\n" << endl;
+}
+
+
+bool isPrime(long long n) {
+    // Check if n is prime
+    for (int i = 2; i <= n / 2; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
